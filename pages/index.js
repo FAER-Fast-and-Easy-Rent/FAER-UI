49abc3e5637d68390a0e5a72e5a13c7d672a223d
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2021-05-31 13:27:35 +0000

    all initial setup done along with landing page

--- pages/index.js
+++ pages/index.js
@@ -2,81 +2,73 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 text-white bg-gradient-to-br from-fuchsia-500 via-purple-600 to-violet-500">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>FAER | A RENTAL SYSTEM </title>
+        <link rel="icon" href="/favicon.png" />
+        <meta
+          name="title"
+          content="FAER | A RENTAL SYSTEM"
+        />
+        <meta
+          name="description"
+          content="FAER | A RENTAL SYSTEM"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://faer.vercel.app/"
+        />
+        <meta
+          property="og:title"
+          content="FAER | A RENTAL SYSTEM"
+        />
+        <meta
+          property="og:description"
+          content="FAER | A RENTAL SYSTEM"
+        />
+        <meta
+          property="og:image"
+          content="https://faer.vercel.app/landing-page.png"
+        />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://faer.vercel.app"
+        />
+        <meta
+          property="twitter:title"
+          content="FAER | A RENTAL SYSTEM"
+        />
+        <meta
+          property="twitter:description"
+          content="FAER | A RENTAL SYSTEM"
+        />
+        <meta
+          property="twitter:image"
+          content="https://faer.vercel.app/landing-page.png"
+        />
+        <link
+          rel="canonical"
+          href="https://faer.vercel.app/"
+        />
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+        <h1 className=" text-9xl font-black">
+         FAER
         </h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
+        <p className="mt-3 text-2xl uppercase tracking-widest">
+          Fast and Easy Rent
+        </p>
+        <p className="py-10 text-xl tracking-widest underline">
+          !!! Launching Soon !!!
         </p>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
     </div>
   )
 }
