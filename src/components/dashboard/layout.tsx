ef214cd76ef374ca0e3878dab4aafae4bc27ff64
bhimrazy <bhimrajyadav977@gmail.com>
2022-02-04 19:38:20 +0545

    Added logout functionality

---
+++ src/components/dashboard/layout.tsx
@@ -0,0 +1,44 @@
+import React, { ReactNode } from "react";
+import Head from "next/head";
+import Header from "./header";
+import Footer from "./footer";
+
+export default function Layout({ children, title, user }: { children: ReactNode, title: string, user: any }) {
+    const page_title = title ? title + " | FAER | A RENTAL SYSTEM" : "FAER | A RENTAL SYSTEM";
+    return (
+        <div className="flex font-poppins flex-col antialiased bg-gray-300">
+            <Head>
+                <title>{page_title}</title>
+                <link rel="icon" href="/favicon.png" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+                    rel="stylesheet"
+                />
+                <meta name="title" content="FAER | A RENTAL SYSTEM" />
+                <meta name="description" content="FAER | A RENTAL SYSTEM" />
+
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://faer.vercel.app/" />
+                <meta property="og:title" content="FAER | A RENTAL SYSTEM" />
+                <meta property="og:description" content="FAER | A RENTAL SYSTEM" />
+                <meta
+                    property="og:image"
+                    content="https://faer.vercel.app/landing-page.png"
+                />
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content="https://faer.vercel.app" />
+                <meta property="twitter:title" content="FAER | A RENTAL SYSTEM" />
+                <meta property="twitter:description" content="FAER | A RENTAL SYSTEM" />
+                <meta
+                    property="twitter:image"
+                    content="https://faer.vercel.app/landing-page.png"
+                />
+                <link rel="canonical" href="https://faer.vercel.app/" />
+            </Head>
+            <Header user={user} />
+            {children}
+            <Footer />
+        </div>
+    );
+}
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,18 @@
 import { GetServerSideProps } from 'next'
+import Layout from 'src/components/dashboard/layout';
+import axios from 'src/lib/axios';
 
-export default function Account() {
-  return <div>Welcome to dashboard</div>;
+export default function Account({ data }) {
+  const page = {
+    title: "Dashboard",
+  };
+  return (
+    <Layout user={data?.user} title={page?.title}>
+      <main className="flex flex-col">
+        {/* <HeroSection /> */}
+      </main>
+    </Layout>
+  );
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
@@ -10,7 +21,15 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
     res.statusCode = 302
     res.setHeader('Location', '/login')
   }
+  const userConfig = {
+    headers: {
+      'Authorization': 'Bearer ' + access
+    }
+  }
+
+  const { data } = await axios.get(process.env.API_URL + '/api/v1/auth/user/me', userConfig)
+  console.log(data)
   return {
-    props: {},
+    props: { data },
   }
 }
